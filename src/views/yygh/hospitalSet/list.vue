ddd4e5040693a28d02642ba38fe6ddc3974c14be
SkiroNako <2681678536@qq.com>
2023-04-11 14:31:35 +0800

    分页列表

--- src/views/yygh/hospitalSet/list.vue
+++ src/views/yygh/hospitalSet/list.vue
@@ -1,12 +1,34 @@
 <template>
     <div class="app-container">
-        医院设置列表...
+        <el-table :data="list" style="width: 100%">
+            <el-table-column prop="hosname" label="医院名称" width="180">
+            </el-table-column>
+            <el-table-column prop="hoscode" label="医院编号" width="180">
+            </el-table-column>
+            <el-table-column prop="contactsName" label="联系人姓名" width="180">
+            </el-table-column>
+            <el-table-column prop="contactsPhone" label="联系人手机" width="180">
+            </el-table-column>
+            <el-table-column label="操作">
+                <template slot-scope="scope">
+                    <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                    <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
 
+        <div class="block">
+            <span class="demonstration">完整功能</span>
+            <el-pagination @current-change="fetchData"
+                :current-page="page" :page-sizes="[100, 200, 300, 400]" :page-size="size"
+                layout="total, prev, pager, next, jumper" :total="total">
+            </el-pagination>
+        </div>
     </div>
 </template>
   
 <script>
-import  hosptialSet  from '@/api/hosptialSet'
+import hosptialSet from '@/api/hosptialSet'
 
 export default {
     data() {
@@ -14,7 +36,8 @@ export default {
             list: null,
             listLoading: true,
             page: 1,
-            size: 10,
+            size: 3,
+            total: 0,
             hosptialSetQuery: {}
         }
     },
@@ -22,10 +45,15 @@ export default {
         this.fetchData()
     },
     methods: {
-        fetchData() {
+        fetchData(page = 1) { //ES6 page值未分配
+            this.page = page;
+
             //分页条件查询
             hosptialSet.pageList(this.page, this.size, this.hosptialSetQuery).then(resopnse => {
-                console.log(resopnse);
+                this.list = resopnse.data.hospitalSetList;
+                this.total = resopnse.data.total;
+
+                this.listLoading = false;   //结束加载
             })
         }
     }
